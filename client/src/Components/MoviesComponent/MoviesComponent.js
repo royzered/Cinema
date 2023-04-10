1e1b6567872d5e103a8869d9a7d9b7f19d5cdace
ROY <zroy2000@gmail.com>
2023-04-11 00:10:58 +0300

    Add search function to Movies Component, modify CSS and Movie Component

--- client/src/Components/MoviesComponent/MoviesComponent.js
+++ client/src/Components/MoviesComponent/MoviesComponent.js
@@ -1,10 +1,11 @@
 import '../../App.css';
 import utils  from '../../API/utils';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 function MoviesComponent() {
+  let addFilmIcon = "https://www.freepnglogos.com/uploads/plus-icon/add-plus-icon-28.png";
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -13,6 +14,7 @@ function MoviesComponent() {
   const movieStoreData = useSelector(state => state.movies);
   const subStoreData = useSelector(state => state.subs);
 
+  const [search, setSearch] = useState([]);
 
 
 useEffect(() => {
@@ -24,12 +26,11 @@ useEffect(() => {
   checkToken(token);
 }, [token] );
 
-
-
 useEffect(() => {
 
   async function getMovies() {
       let moviesFromServer = await utils.getMovies();
+      setSearch(moviesFromServer.data)
       dispatch({type : "GETDATA", payload : moviesFromServer.data})
   }
   
@@ -40,6 +41,7 @@ useEffect(() => {
   
   getMovies();
   getSubs();
+  
 
   },[]);
 
@@ -55,7 +57,13 @@ useEffect(() => {
     dispatch({type : "DELETEMOVIE", payload : id})
   };
 
-  
+  const searchFilm = (input) => {
+    return setSearch(movieStoreData.movies.filter(movie => {
+      const movieName = movie.filmName.toLowerCase();
+      const searchInput = input.toLowerCase();
+      return movieName.includes(searchInput);
+    }));
+  }
 
 
   return (
@@ -64,17 +72,18 @@ useEffect(() => {
       <h2 style={{background : "lime", color : 'black', fontSize: "32px"}}>
       Films Playing
       </h2>
-      <input className='searchInput' type='text' placeholder='Search'/> 
+      <input onKeyUpCapture={(e) => {searchFilm(e.target.value)}} onClick={(e) => { searchFilm(e.target.value); e.target.value = '';}} className='searchInput' type='text' placeholder='Search'/> 
+      <Link title="Add Film" style={{fontSize : "30px", position : 'absolute', width : "2%", top: "9.5%", right : "2%"}}> + </Link>
       </span>
       <table>
         {
-        movieStoreData.movies.map(movie => {
+        search.map(movie => {
            return( <tbody> <tr>
             <td>
               <img className='moviePoster' src={movie.image} alt={movie.name} />
             </td>
             <td style={{fontWeight : "bold"}}>
-             <Link to={`movie/${movie._id}`}> {movie.filmName} ({movie.released})  </Link>
+             <Link to={`movie/${movie._id}`}> <span className='filmNameSpan'>{movie.filmName}</span> ({movie.released})  </Link>
             </td>
             <td>
                <ul>
